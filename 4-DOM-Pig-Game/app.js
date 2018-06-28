967eec2d4cd8115d112982229531232de957b00d
Albireo20 <Stephan20.12@googlemail.com>
2018-06-28 14:41:50 +0200

    app.js update

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -31,6 +31,26 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	diceDOM.src = 'dice-' + dice + '.png';
 	
 	// 3. Update Round Score, only IF the rolled number was NOT 1!
+	if (dice !== 1) {
+		//add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//Next Player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;  //Turnary Operator
+		roundScore = 0;
+		
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+		
+		diceDOM.style.display = 'none';
+	}
 });
 
 
